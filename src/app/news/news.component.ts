946fc431cc877e60d91b51fe162d0a51c407bdbf
F1nqer <RetroLOL2>
2020-11-28 17:37:42 +0300

    E

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Computer} from '../shared/computer';
+import {NewsService} from '../news.service';
+import {News} from '../shared/news';
+import {concat, from, of} from 'rxjs';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
-
+  news: News[];
+  new: News[];
+  ids: number[];
+  errMsg: string;
+  constructor(private newsService: NewsService) { }
+  // tslint:disable-next-line:typedef
   ngOnInit(): void {
+    this.newsService.getNews()
+      .subscribe(news => this.news = news,
+        error => this.errMsg = error);
   }
+  // tslint:disable-next-line:typedef
+  magic(){
+    const obs = from(this.news).pipe(filter(n => n.id % 2 !== 0));
+  }
+  // tslint:disable-next-line:typedef
+  // magicconcat(){
+  //   const obs = from(this.news).pipe(concat())
+  //   this.new = this.news.concat();
+  //   this.news = this.new;
+  // }
 
 }
